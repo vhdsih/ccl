1e1baee5f4f5bd7dcdb4225c4a278616aa40e7e9
vhdsih <vhdsih@hotmail.com>
2022-06-27 17:57:02 +0800

    add define rule

--- include/utils/log.h
+++ include/utils/log.h
@@ -8,20 +8,29 @@
 
 #include <ctime>
 
+#ifndef logger
 #define logger(...)                    \
     printf("[I]%ld: ", time(nullptr)); \
     printf(__VA_ARGS__);               \
     printf("\n");
 
+#endif
+
+#ifndef logger_error
 #define logger_error(...)              \
     printf("[E]%ld: ", time(nullptr)); \
     printf(__VA_ARGS__);               \
     printf("\n");
 
+#endif
+
+#ifndef logger_warn
 #define logger_warn(...)               \
     printf("[W]%ld: ", time(nullptr)); \
     printf(__VA_ARGS__);               \
     printf("\n");
+
+#endif
 
 namespace ccl {} // namespace ccl
 
